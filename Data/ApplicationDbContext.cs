6f236220f836a376b5c7b629883290ed5dcdd475
RaneemDaqa <raneemdaqa9@gmail.com>
2024-08-24 22:15:26 +0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using firstCode.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstCode.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ASP9_EF;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
